303b87124f25c648302625e2f0e22ae1da3289eb
Garrett Sanders <sgarrettsanders@gmail.com>
2017-05-17 11:21:30 -0400

    added font loader for self-hosting

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ import { AppContainer } from 'react-hot-loader';
 
 import App from './components/App';
 
+import './index.css';
+
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
