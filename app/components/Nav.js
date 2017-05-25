438090d24e0bd5f7f6bc435c98acd884665a1126
Garrett Sanders <sgarrettsanders@gmail.com>
2017-05-25 10:17:05 -0400

    Add nav icons and hover elevations

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -7,8 +7,22 @@ class Nav extends React.Component {
       <nav className='nav'>
         <ul>
           <li className='nav__item logo'>Logo</li>
-          <li className='nav__item menu'>Menu</li>
-          <li className='nav__item social'>Social</li>
+          <li className='nav__item projects'>
+            <i className='fa fa-folder-o'></i>
+          </li>
+          <li className='nav__item menu'>
+            <div className='nav__navigation'>
+              <i className='fa fa-chevron-left'></i>
+              <i className='fa fa-chevron-right'></i>
+              <span className='project--next'>Next: Pella Marketing Brochure</span>
+            </div>
+          </li>
+          <li className='nav__item social'>
+            <i className='fa fa-envelope-o'></i>
+            <i className='fa fa-twitter'></i>
+            <i className='fa fa-instagram'></i>
+            <i className='fa fa-behance'></i>
+          </li>
         </ul>
       </nav>
     )
